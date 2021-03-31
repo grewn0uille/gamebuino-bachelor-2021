aa37d7dff6b024c750a95329bea10d685adf6f37
Lucie Anglade <lucie@courtbouillon.org>
2021-03-31 16:55:17 +0200

    On change la couleur du texte et du fond, la taille du texte. Et on écrit sur plusieurs lignes

--- structure-de-base.ino
+++ structure-de-base.ino
@@ -8,5 +8,9 @@ void loop() {
   while(!gb.update());
   gb.display.clear();
 
-  gb.display.print("hello, world");
+  gb.display.setColor(RED, YELLOW);
+
+  gb.display.setFontSize(2);
+
+  gb.display.print("hello, world\nbonjour, monde\nhola, mundo");
 }
